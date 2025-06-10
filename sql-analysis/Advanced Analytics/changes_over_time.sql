0e6501173e3488400a15f72e1d46fc06b06ddb42
samirthapapkr <samirthapapkr859@gmail.com>
2025-06-11 04:11:06 +1000

    Added sql scripts and documents

--- sql-analysis/Advanced Analytics/changes_over_time.sql
+++ sql-analysis/Advanced Analytics/changes_over_time.sql
@@ -8,7 +8,7 @@ Purpose:
 - To measure growth or decline over specific periods.
 
 SQL Functions Used:
-- Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+- Date Functions: EXTRACT(), TO_CHAR()
 - Aggregate Functions: SUM(), COUNT(), AVG()
 =================================
 */
@@ -43,38 +43,4 @@ order_month
 ORDER BY
 MIN(order_date);
 
-/*
-=========================================================================
-Cumulative Analysis
-=========================================================================
-Purpose:
-- To calculate running totals or moving averages for key metrics.
-- To track performance over time cumulatively.
-- Useful for growth analysis or identifying long-term trends.
 
-SQL Functions Used:
- - Window Functions: SUM() OVER(), AVG() OVER()
-========================================================================
-*/
-
--- Calculate the total sales and average price per month
--- and the running total of sales over time
-SELECT
-order_date,
-total_sales,
--- Partition for the year
---SUM(total_sales) OVER (PARTITION BY order_date ORDER BY order_date) AS running_total_sales
-SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
-ROUND(AVG(avg_price) OVER (ORDER BY order_date), 2) AS moving_average_price
-FROM
-(	
-SELECT
-TO_CHAR(DATE_TRUNC('year', order_date), 'YYYY-MM') AS order_date, 
-SUM(sales_amount) AS total_sales,
-ROUND(AVG(price),2) AS avg_price
-FROM gold.fact_sales
-WHERE order_date IS NOT NULL
-GROUP BY TO_CHAR(DATE_TRUNC('year', order_date),'YYYY-MM')
-ORDER BY
-MIN(order_date)
-)t
--- sql-analysis/Data_Analysis.md
+++ sql-analysis/Data_Analysis.md
@@ -1,6 +1,6 @@
 # Superstore Data Analysis
 
-A comprehensive collection of SQL scripts for data exploration, analytics and reporting, These scripts cover various analysis such as database exploration, measures and metrics, time-based trends, cumulative analytics, segmentation and more. Each script focueses on a specific analytical theme and demonstrates best practices for SQL queries.
+A comprehensive collection of SQL scripts for data exploration, analytics and reporting, These scripts cover various analysis such as database exploration, measures and metrics, time-based trends, cumulative analytics, segmentation and more. Each script focuses on a specific analytical theme and demonstrates best practices for SQL queries.
 
 
 ## Repository Structure
